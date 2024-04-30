b2260b4759fde46dc284b57ade442728b7ecd6e5
[Zoltai Tamás] <[zoltai.tamas@students.jedlik.eu]>
2024-04-30 11:53:43 +0200

    Python 1. feladat

---
+++ python/1_feladat.py
@@ -0,0 +1,9 @@
+r = float(input('Adja meg a kör sugarát: '))
+d = 2*r
+k = 2*r*3.14
+t = r*r*3.14
+
+print(f'\n{r} sugarú kör:')
+print(f'\tÁtmérő: {d:.2f}')
+print(f'\tKerület: {k:.2f}')
+print(f'\tTerület: {t:.2f}')
